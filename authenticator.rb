ff614c3e92048f86dfe71d3970193fc584b0c4d6
Wilfred <wilfredbayudan@gmail.com>
2022-05-11 22:52:42 -1000

    Init commit

---
+++ authenticator.rb
@@ -0,0 +1,10 @@
+users = [
+  { username: 'wilfred', password: 'test1234' },
+  { username: 'bayudan', password: '1234test' }
+]
+
+puts 'Welcome to the authenticator'
+25.times { print '-' }
+puts 'This program will take input from the user and compare password'
+puts 'If password us correct, you will get back the user object'
+25.times { print '-' }
